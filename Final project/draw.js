86c4fa786925a56602cfa68f9eb6c364c62535bd
e-yankovich <yankovich.evgeniya@gmail.com>
2022-11-18 21:22:07 +0600

    add new functionality

--- Final project/draw.js
+++ Final project/draw.js
@@ -10,10 +10,89 @@ class Block {
 
     update(){
         this.element.id = this.id;
-        this.elemen.style.top = this.top;
+        this.element.style.top = this.top;
         this.element.style.left = this.left;
         this.element.style.width = this.width;
-        this.elemen.style.height = this.height;
+        this.element.style.height = this.height;
+    }
+
+    adapt(widthProportion, heightProportion, prevTop, count){
+        this.width = parseFloat(this.width)/widthProportion;
+        this.height = parseFloat(this.height)/heightProportion;
+        this.top = parseFloat(prevTop)-2*parseFloat(this.height);
+        this.left = parseFloat(this.left)/widthProportion;
+        this.update();
+
+        let rectangle = this.element.getElementById("rect" + count);
+        rectangle.style.width = parseFloat(this.width)/widthProportion;
+        rectangle.style.height = parseFloat(this.height)/heightProportion;
+    }
+
+    move(containerWidth, game) {
+        let width = parseFloat(this.width);
+        let left = parseFloat(this.left);
+
+        if ((left + game.get_blockVelocity() + width)>=containerWidth){
+            game.reverse_blockVelocity();
+            this.left = (containerWidth - width)+"px";
+        }
+
+        if ((left + game.get_blockVelocity())<0){
+            game.reverse_blockVelocity();
+            this.left = "0px";
+        }
+        
+        this.left = left+game.get_blockVelocity();
+        this.update();
+    }
+
+    crop(init_left, init_width){
+        let current_left = parseFloat(this.left);
+        let current_width = parseFloat(this.width);
+        if (current_left+current_width <= init_left || current_left>= init_left+init_width){
+            stopTick();
+            stopGame();
+            return;
+        }
+        let ratio = 0;
+        if (current_left > init_left){
+            let newWidth = current_width+(init_left-current_left);
+            if (newWidth<1){
+                newWidth+=1;
+            }
+            this.width = newWidth;
+            ratio = (this.width+(init_left-current_left))/init_width;
+            this.update();
+        }
+        if (init_left > current_left){
+            let newWidth = current_width-(init_left-current_left);
+            if (newWidth<1){
+                newWidth+=1;
+            }
+            this.width = newWidth;
+            this.left = current_left+(init_left-current_left);
+            ratio = (this.width-(init_left-this.left))/init_width;
+            this.update();
+        }
+        return ratio;
+    }
+
+    propagate_element(){
+        this.element = document.getElementById(this.id);
+    }
+
+    propagate_params(){
+        this.id = this.element.id;
+        this.top = this.element.style.top;
+        this.left = this.element.style.left;
+        this.width = this.element.style.width;
+        this.height = this.element.style.height;
+    }
+
+    init_rect(count, color){
+        let rect = this.element.getElementsByTagName("rect")[0];
+        rect.id = "rect" +count;
+        rect.setAttribute("fill", color);
     }
 }
 
@@ -116,55 +195,62 @@ class MrkpHlpr {
             element.style.display = "block";
         }
     }
+
+    flashAndFocus(input, colors, fnf=null, period=150, count=4){
+
+        if (fnf == null)
+            fnf = this.flashAndFocus;
+
+        setTimeout(
+            function()
+            {
+                input.style.backgroundColor = colors[(count%2)];
+                if (count > 1){
+                    fnf(input, colors, fnf, period, count-1);
+                }
+                else {
+                    input.focus();
+                }
+            }, 
+            period
+        );
+
+    }
+
+
 }
 
 let game = new Game();
 let helper = new MrkpHlpr()
 
 function startGame(event){
+
+    event = event || window.event;
+
     game.set_score()
     document.getElementById("currentScore").innerText = "0";
     input = document.getElementById("playerName");
     let inputedPlayerName = input.value;
     if (inputedPlayerName.trim()== ""){
-        flashAndFocus(input);
+        helper.flashAndFocus(input, ['#fca2c1', 'white']);
         return;
     }
+    if (inputedPlayerName.length > 15){
+        document.getElementById("warning").style.display = "block";
+        helper.flashAndFocus(input, ['#fca2c1', 'white']);
+        return;
+    }
+    document.getElementById("warning").style.display = "none";
     game.set_username(escape(input.value.trim()));
     event.stopPropagation();
     for (let i = 1; i <game.blocks.length; i++){
-        game.blocks[i].parentElement.removeChild(game.blocks[i]);
+        game.blocks[i].element.parentElement.removeChild(game.blocks[i].element);
     }
     game.blocks = game.blocks.slice(0,1);
     game.set_gameStatus(true);
     addBlock();
     let ot = document.getElementById("overlayText");
     helper.toggle_element(ot);
-}
-
-function flashAndFocus(input){
-    input.style.backgroundColor = "#fca2c1";
-    setTimeout(function()
-    {
-        input.style.backgroundColor = "white";
-        setTimeout(function()
-        {
-            input.style.backgroundColor = "#fca2c1";
-            setTimeout(function()
-            {
-                input.style.backgroundColor = "white";
-                setTimeout(function()
-                {
-                    input.style.backgroundColor = "#fca2c1";
-                    setTimeout(function()
-                    {
-                        input.style.backgroundColor = "white";
-                        input.focus();
-                    }, 150);
-                }, 150);
-            }, 150);
-        }, 150);
-    }, 150);
 }
 
 function initPositionDonor(resize = false){
@@ -185,46 +271,47 @@ function initPositionDonor(resize = false){
     rect.style.height = rectHeight;
     let rect_x = (pf_width - rectWidth)/2;
     let rect_y = pf_height - rectHeight;
-    let svg = document.getElementById("donor");
+    let block = new Block();
+    block.id = "donor";
+    block.propagate_element();
+    helper.toggle_element(block.element, false);
+    block.top = rect_y;
+    block.left = rect_x;
+    block.width = rectWidth;
+    block.height = rectHeight;
+    block.update();
     if (!resize){
-        game.blocks.push(svg);
+        game.blocks.push(block);
     }
-    svg.style.top = rect_y;
-    svg.style.left = rect_x; 
-    svg.style.display = "block";
-    svg.style.width = rectWidth;
-    svg.style.height = rectHeight;
 }
 
 function addBlock(){
+    if (game.blocks.length == 24){
+        stopTick();
+        stopGame();
+        return;
+    }
     let donor = game.blocks.at(-1); 
-    newBlock = donor.cloneNode(true);
+    let newBlock = new Block();
+    newBlock.element = donor.element.cloneNode(true);
     newBlock.id = "block" + game.blocks.length;
-    let rect = newBlock.getElementsByTagName("rect")[0];
-    rect.id = "rect" + game.blocks.length;
-    rect.setAttribute("fill", game.get_colors()[game.blocks.length%game.get_colors().length])
+    newBlock.init_rect(game.blocks.length, game.get_colors()[game.blocks.length%game.get_colors().length])
+
     game.blocks.push(newBlock);
-    donor.parentElement.appendChild(newBlock);
-    newBlock.style.top = parseFloat(newBlock.style.top) - 2*parseFloat(newBlock.style.height);
-    //newBlock.style.left = parseFloat(newBlock.style.left) + 20;
-    newBlock.style.left = 0;
+    donor.element.parentElement.appendChild(newBlock.element);
+    newBlock.propagate_params()
+    newBlock.top = parseFloat(newBlock.top) - 2*parseFloat(newBlock.height);
+    newBlock.left = 0;
+    newBlock.update();
     game.set_animationStatus(true);
     requestAnimationFrame(tick);
 }
 
 function adaptBlocks(){
-    let pf = document.getElementById("play_field");
-    let pf_height = pf.offsetHeight;
-    let pf_width = pf.offsetWidth;
     for (let i = 1; i <game.blocks.length; i++){
-        game.blocks[i].style.width = parseFloat(game.blocks[i].style.width)/game.get_widthProportion();
-        game.blocks[i].style.height = parseFloat(game.blocks[i].style.height)/game.get_heightProportion();
-        let rectaingle = game.blocks[i].getElementById("rect" + i);
-        rectaingle.style.width = parseFloat(game.blocks[i].style.width)/game.get_widthProportion();
-        rectaingle.style.height = parseFloat(game.blocks[i].style.height)/game.get_heightProportion();
-        game.blocks[i].style.top = parseFloat(game.blocks[i-1].style.top)-2*parseFloat(game.blocks[i].style.height);
-        //game.blocks[i].style.top = parseFloat(game.blocks[i].style.top)/game.get_heightProportion();
-        game.blocks[i].style.left = parseFloat(game.blocks[i].style.left)/game.get_widthProportion();
+        let wp = game.get_widthProportion(); 
+        let hp = game.get_heightProportion();
+        game.blocks[i].adapt(wp, hp, game.blocks[i-1].element.style.top, i);
     }
 }
 
@@ -239,20 +326,8 @@ function tick() {
     let pf = document.getElementById("play_field");
     let pfWidth = pf.offsetWidth;
     let activeBlock = game.blocks.at(-1);
-    let activeBlockWidth = parseFloat(activeBlock.style.width);
-    let activeBlocLeft = parseFloat(activeBlock.style.left);
-
-    if ((activeBlocLeft + game.get_blockVelocity() + activeBlockWidth)>=pfWidth){
-        game.reverse_blockVelocity();
-        activeBlock.style.left = (pfWidth - activeBlockWidth)+"px";
-    }
-
-    if ((activeBlocLeft + game.get_blockVelocity())<0){
-        game.reverse_blockVelocity();
-        activeBlock.style.left = "0px";
-    }
+    activeBlock.move(pfWidth, game);
     
-    activeBlock.style.left = activeBlocLeft+game.get_blockVelocity();
     if (game.get_animationStatus()){
         requestAnimationFrame(tick);
     }
@@ -274,41 +349,24 @@ function stopGame(){
     helper.toggle_element(ot, false)
     let un = document.getElementById("userName");
     un.style.display = "block";
-
+    let go = document.getElementById("gameOver");
+    go.style.display = "block";
     getScores();
     sendScores();
 }
 
 function cropBlock(){
-    let init_left = parseFloat(game.blocks.at(-2).style.left);
-    let init_width = parseFloat(game.blocks.at(-2).style.width);
+    let init_left = parseFloat(game.blocks.at(-2).left);
+    let init_width = parseFloat(game.blocks.at(-2).width);
     let block = game.blocks.at(-1);
-    let current_left = parseFloat(block.style.left);
-    let current_width = parseFloat(block.style.width);
+    let current_left = parseFloat(block.left);
+    let current_width = parseFloat(block.width);
     if (current_left+current_width <= init_left || current_left>= init_left+init_width){
         stopTick();
         stopGame();
         return;
     }
-    let ratio = 0;
-    if (current_left > init_left){
-        let newWidth = parseFloat(block.style.width)+(init_left-current_left);
-        if (newWidth<1){
-            newWidth+=1;
-        }
-        block.style.width = newWidth;
-        ratio = (parseFloat(block.style.width)+(init_left-current_left))/init_width;
-
-    }
-    if (init_left > current_left){
-        let newWidth = parseFloat(block.style.width)-(init_left-current_left);
-        if (newWidth<1){
-            newWidth+=1;
-        }
-        block.style.width = newWidth;
-        block.style.left = parseFloat(block.style.left)+(init_left-current_left);
-        ratio = (parseFloat(block.style.width)-(init_left-current_left))/init_width;
-    }
+    let ratio = block.crop(init_left, init_width);
     if(ratio<0){
         ratio = 1 + ratio;
     }
@@ -345,14 +403,23 @@ function showScores(data) {
         alert(data.error);
     else {
         if ( data.result!="" ) { 
-            leaderBoard=JSON.parse(data.result);
+            game.leaderBoard=JSON.parse(data.result);
             updateLeaderBoard();
         }
     }
 }
 
 function updateLeaderBoard(){
-    document.getElementById("leaderboard").innerText = JSON.stringify(leaderBoard);
+    //document.getElementById("leaderboard").innerText = JSON.stringify(game.leaderBoard);
+    for (let i=1; i<=3; i++){
+        if (game.leaderBoard.length >= i && game.leaderBoard[i-1].score>0){
+            document.getElementById("leaderboard" + i).innerText = game.leaderBoard[i-1].name + ": " + game.leaderBoard[i-1].score;
+        }
+        else {
+            document.getElementById("leaderboard" + i).innerText = "---"
+        }
+    }
+    document.getElementById("currentRecord").innerText = game.leaderBoard.length >= 0 ? game.leaderBoard[0].score : "---";
 }
 
 function sendScores() {
@@ -369,21 +436,19 @@ function sendScores() {
     );
 }
 
-// сообщения получены, добавляет, показывает, сохраняет
 function lockGetReady(data) {
     if ( data.error!=undefined )
         alert(data.error);
     else {
         if ( data.result!="" ) { 
-            leaderBoard=JSON.parse(data.result);
+            game.leaderBoard=JSON.parse(data.result);
         }
 
         //push to a new array then slice
         let new_scores = []
-        let new_count = leaderBoard.length < 3 ? leaderBoard.length + 1 : 3;
+        let new_count = game.leaderBoard.length < 3 ? game.leaderBoard.length + 1 : 3;
         let push = true;
-        leaderBoard.forEach(element => {
-            alert(JSON.stringify(element))
+        game.leaderBoard.forEach(element => {
             if (game.get_score() >= parseInt(element.score) && push){
                 new_scores.push({"name":game.get_username(),"score":game.get_score().toString()});
                 push = !push;
@@ -393,13 +458,13 @@ function lockGetReady(data) {
         if (push){
             new_scores.push({"name":game.get_username(),"score":game.get_score().toString()});
         }
-        leaderBoard = new_scores.slice(0, new_count);
+        game.leaderBoard = new_scores.slice(0, new_count);
         
         $.ajax( {
                 url : game.get_ajaxHandlerScript(),
                 type : 'POST', dataType:'json',
                 data : { f : 'UPDATE', n : game.get_containerName(),
-                    v : JSON.stringify(leaderBoard), p : updatePassword },
+                    v : JSON.stringify(game.leaderBoard), p : updatePassword },
                 cache : false,
                 success : updateReady,
                 error : errorHandler
